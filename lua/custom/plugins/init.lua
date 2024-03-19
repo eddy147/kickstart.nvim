1b7ba13a80d77d0457307400952157cf16af4d43
eddy147 <eddy147@gmail.com>
2024-03-19 12:39:47 +0100

    Added autosave

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -69,6 +69,15 @@ return {
   },
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle",                       -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
   },
 }
